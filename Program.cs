78d80f763ef3027ff2cc5f6c971cbb858d59445e
marlon-Arauj0 <marlonaraujo1303@gmail.com>
2025-10-07 21:41:17 -0300

    Funcao trigonometricas

--- Program.cs
+++ Program.cs
@@ -6,3 +6,7 @@ calc.Somar(1, 3);
 calc.Subtrair(3, 9);
 calc.Dividir(10, 2);
 calc.Multiplicar(9, 2);
+calc.Potencial(3, 3);
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30);
--- models/Calculator.cs
+++ models/Calculator.cs
@@ -26,5 +26,32 @@ namespace estudosDotnet.models
         {
             Console.WriteLine($"{x} * {y} = {x * y}");
         }
+
+        public void Potencial(int x, int y)
+        {
+            double power = Math.Pow(x, y);
+            Console.WriteLine($"{x} ^ {y} = {power}");
+        }
+
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo} = {Math.Round(seno, 4)}");
+        }
+
+        public void Coseno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double coseno = Math.Cos(radiano);
+            Console.WriteLine($"Coseno de {angulo} = {Math.Round(coseno, 4)}");
+        }
+
+        public void Tangente(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double tangente = Math.Tan(radiano);
+            Console.WriteLine($"Tangente de {angulo} = {Math.Round(tangente, 4)}");
+        }
     }
 }
